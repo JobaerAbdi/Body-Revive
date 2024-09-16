3523ceb7417a970dfff85ca0939c2556a9223ffb
Kamrul Hassan <kamrulthedev@gmail.com>
2024-09-16 23:22:52 +0600

    first commit

---
+++ src/pages/home/components/Gallery.tsx
@@ -0,0 +1,117 @@
+import img1 from "../../../assets/images/h1.webp";
+import img2 from "../../../assets/images/h2.webp";
+import img3 from "../../../assets/images/h3.webp";
+import img4 from "../../../assets/images/h4.webp";
+import img5 from "../../../assets/images/h5.webp";
+import img6 from "../../../assets/images/h6.webp";
+import img7 from "../../../assets/images/h7.webp";
+import img8 from "../../../assets/images/h8.webp";
+import img9 from "../../../assets/images/h9.webp";
+import img10 from "../../../assets/images/h10.webp";
+import img11 from "../../../assets/images/h12.jpg";
+import img13 from "../../../assets/images/h15.jpg";
+import SectionTitle from "@/components/shared/SectionTitle";
+
+const Gallery = () => {
+  return (
+    <div>
+      <SectionTitle title="gallery" subTitle="our healthy customers" />
+      <div className="photo-gallery flex lg:flex-row flex-col gap-5 lg:px-auto px-4">
+        <div className="column">
+          <div className="photo">
+            <img
+              src={img1}
+              alt={`Gallery image`}
+              className="max-w-full w-full h-auto object-cover align-middle inline-block rounded-md"
+            />
+          </div>
+          <div className="photo">
+            <img
+              src={img8}
+              alt={`Gallery image`}
+              className="max-w-full w-full h-auto object-cover align-middle inline-block rounded-md"
+            />
+          </div>
+          <div className="photo">
+            <img
+              src={img3}
+              alt={`Gallery image`}
+              className="max-w-full w-full h-auto object-cover align-middle inline-block rounded-md"
+            />
+          </div>
+        </div>
+        <div className="column">
+          <div className="photo">
+            <img
+              src={img4}
+              alt={`Gallery image`}
+              className="max-w-full w-full h-auto object-cover align-middle inline-block rounded-md"
+            />
+          </div>
+          <div className="photo">
+            <img
+              src={img5}
+              alt={`Gallery image`}
+              className="max-w-full w-full h-auto object-cover align-middle inline-block rounded-md"
+            />
+          </div>
+          <div className="photo">
+            <img
+              src={img6}
+              alt={`Gallery image`}
+              className="max-w-full w-full h-auto object-cover align-middle inline-block rounded-md"
+            />
+          </div>
+        </div>
+        <div className="column">
+          <div className="photo">
+            <img
+              src={img7}
+              alt={`Gallery image`}
+              className="max-w-full w-full h-auto object-cover align-middle inline-block rounded-md"
+            />
+          </div>
+          <div className="photo">
+            <img
+              src={img2}
+              alt={`Gallery image`}
+              className="max-w-full w-full h-auto object-cover align-middle inline-block rounded-md"
+            />
+          </div>
+          <div className="photo">
+            <img
+              src={img9}
+              alt={`Gallery image`}
+              className="max-w-full w-full h-auto object-cover align-middle inline-block rounded-md"
+            />
+          </div>
+        </div>
+        <div className="column">
+          <div className="photo">
+            <img
+              src={img13}
+              alt={`Gallery image`}
+              className="max-w-full w-full h-auto object-cover align-middle inline-block rounded-md"
+            />
+          </div>
+          <div className="photo">
+            <img
+              src={img11}
+              alt={`Gallery image`}
+              className="max-w-full w-full h-auto object-cover align-middle inline-block rounded-md"
+            />
+          </div>
+          <div className="photo">
+            <img
+              src={img10}
+              alt={`Gallery image`}
+              className="max-w-full w-full h-auto object-cover align-middle inline-block rounded-md"
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Gallery;
---
+++ src/pages/home/components/Hero.tsx
@@ -0,0 +1,67 @@
+import { Carousel } from "antd";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import b1 from "../../../assets/images/b1.webp";
+import b2 from "../../../assets/images/b2.webp";
+import b3 from "../../../assets/images/b3.webp";
+import { Link } from "react-router-dom";
+
+const Hero = () => {
+  return (
+    <div className="relative w-full">
+      <div className="absolute top-0 left-0 w-full h-full bg-black bg-opacity-40 z-10 flex items-center justify-center md:justify-start lg:justify-start">
+        {/* utils */}
+        <div className="text-center lg:p-14 md:text-left md:ml-16 lg:-mt-20 md:-mt-10">
+          <h1 className="text-white text-xs md:text-sm lg:text-base font-bold">
+            BRING THE CLUB EXPERIENCE HOME
+          </h1>
+          <h2 className="text-white text-xl md:text-3xl lg:text-6xl font-serif mt-4">
+            CLUB SERIES+ <br /> CARDIO EQUIPMENT
+          </h2>
+          <Link
+            to="/products"
+            className="bg-secondaryColor text-white lg:font-bold font-semibold lg:py-3 lg:px-5  py-2 px-4 rounded-sm hover:bg-primaryColor"
+          >
+            Explore our products
+          </Link>
+        </div>
+      </div>
+
+      <Carousel className="w-full h-[300px] md:h-[400px] lg:h-[700px]" autoplay>
+        <div>
+          <img
+            loading="lazy"
+            className="w-full h-[300px] md:h-[400px] lg:h-[700px] object-cover"
+            src="https://i.ibb.co/rQp5mqX/Screenshot-2024-08-18-110818-1.png"
+            alt="Shirtless bodybuilder doing side plank exercise"
+          />
+        </div>
+        <div>
+          <img
+            loading="lazy"
+            className="w-full h-[300px] md:h-[400px] lg:h-[700px] object-cover"
+            src="https://i.ibb.co/G2chXCR/Screenshot-2024-08-18-110839.png"
+            alt="Couple training together in gym"
+          />
+        </div>
+        <div>
+          <img
+            loading="lazy"
+            className="w-full h-[300px] md:h-[400px] lg:h-[700px] object-cover"
+            src="https://i.ibb.co/yhPhGfh/Screenshot-2024-08-18-110831.png"
+            alt="Muscular man doing push-ups with one hand"
+          />
+        </div>
+        <div>
+          <img
+            loading="lazy"
+            className="w-full h-[300px] md:h-[400px] lg:h-[700px] object-cover"
+            src="https://i.ibb.co/C5rmkWG/Screenshot-2024-08-18-110849.png"
+            alt="Young healthy man athlete doing exercise with ropes in gym"
+          />
+        </div>
+      </Carousel>
+    </div>
+  );
+};
+
+export default Hero;
