9a0138023c001b7fe5ac2dbfa5483d61a8d01741
Kamrul Hassan <kamrulthedev@gmail.com>
2024-09-17 07:46:28 +0600

    create-basae-api

--- src/redux/api/baseApi.ts
+++ src/redux/api/baseApi.ts
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 const baseApi = createApi({
   reducerPath: "api",
   baseQuery: fetchBaseQuery({
-    baseUrl: "https://assignment-4-ruddy-one.vercel.app/api",
+    baseUrl: "https://body-revive-server.vercel.app/api",
   }),
   tagTypes: ["Product"],
   endpoints: () => ({}),
